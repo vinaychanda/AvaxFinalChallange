c10dbd0e2ee475c07303bbcb4b502490d76cdce3
vinaychanda <87568227+vinaychanda@users.noreply.github.com>
2023-05-20 12:54:14 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -13,19 +13,28 @@ contract DegenToken is ERC20, Ownable {
     }
 
     function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf(_msgSender()) >= amount, "Insufficient balance");
+        
         _transfer(_msgSender(), recipient, amount);
         return true;
     }
 
     function redeem(uint256 amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf(_msgSender()) >= amount, "Insufficient balance");
+        
         _burn(_msgSender(), amount);
     }
 
     function burn(uint256 amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf(_msgSender()) >= amount, "Insufficient balance");
+        
         _burn(_msgSender(), amount);
     }
 
     function balanceOf(address account) public view override returns (uint256) {
-        return balanceOf(account);
+        return super.balanceOf(account);
     }
 }
